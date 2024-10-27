1f4d43497692d829f1a03b2555f7d1114429be0a
Sytske van Hasselt <53014699+sytskevanhasselt@users.noreply.github.com>
2024-10-27 16:49:06 +0100

    Update conf.py
    Volgens mij is 'ie nu helemaal opgeschoond.

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,9 @@ import sys
 
 sys.path.append(os.path.abspath("_ext"))
 
+
+
+
 # -- Project information -----------------------------------------------------
 
 project = "Test syps"
@@ -35,13 +38,19 @@ copyright = "2024"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+# Volgens mij heb ik iig nodig: sphinx_rtd_theme, én myst_parser (voor markdown)
+# gekopieerd uit Openzaak: "sphinx.ext.todo","recommonmark","sphinx_markdown_tables","sphinx_tabs.tabs"
+
 extensions = [
-    "sphinx.ext.todo",
     "sphinx_rtd_theme",
     "myst_parser",
-    #     "recommonmark",
-    #    "sphinx_markdown_tables",
+    "sphinx.ext.todo",
+    "recommonmark",
+    "sphinx_markdown_tables",
+    "sphinx_tabs.tabs",
 ]
+
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -56,8 +65,8 @@ language = "nl"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 source_suffix = ['.rst', '.md']
 
 # -- Options for HTML output -------------------------------------------------
@@ -68,18 +77,39 @@ source_suffix = ['.rst', '.md']
 html_logo = "logo.png"
 html_theme = "sphinx_rtd_theme"
 
+# onderstaande komt uit: 
+# https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html
+
+html_theme_options = {
+    'logo_only': False,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': True,
+    'style_nav_header_background': 'white',
+    'flyout_display': 'attached',
+    'version_selector': True,
+    'language_selector': True,
+    # Toc options
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False   
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+
 html_static_path = ["_static"]
-html_css_files = [
-    "theme_overrides.css",  # override wide tables with word wrap
-]
 
-todo_include_todos = True
+# ik weet niet zo goed waar deze vandaan komt. En ik heb dus wel een theme_override.css
+# maar ik weet niet at die doet. Dus die ga ik niet gebruiken. 
 
-linkcheck_ignore = [
-    r"https?://.*\.gemeente.nl",
-    r"http://localhost:\d+/",
-    r"https://.*sentry.*",
-]
+# html_css_files = [
+#     "theme_overrides.css",  # override wide tables with word wrap
+# ]
+
+# deze komt ook uit openzaak
+# maar zij gebruiken gegenereerde content. dus deze gebruik ik ook niet
+# todo_include_todos = True
+
